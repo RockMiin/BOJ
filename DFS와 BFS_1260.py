86dafa74d90874425677970dba5cbc50b5837d8c
rockmiin <csm1959@naver.com>
2020-08-24 13:31:26 +0900

    #DFS #BFS

---
+++ DFS와 BFS_1260.py
@@ -0,0 +1,34 @@
+from _collections import deque
+
+def dfs(v):
+    print(v, end=' ')
+    visited[v]= True
+    for e in adj[v]:
+        if not(visited[e]):
+            dfs(e)
+def bfs(v):
+    q= deque([v])
+    while q:
+        v= q.popleft()
+        if not(visited[v]):
+            visited[v]= True
+            print(v, end=' ')
+            for e in adj[v]:
+                if not visited[e]:
+                    q.append(e)
+
+if __name__=='__main__':
+    n, m, v=map(int, input().split())
+    adj= [[] for i in range(n+1)]
+    for i in range(m):
+        x, y= map(int, input().split())
+        adj[x].append(y)
+        adj[y].append(x)
+    for e in adj:
+        e.sort()
+
+    visited= [False]*(n+1)
+    dfs(v)
+    print()
+    visited= [False]*(n+1)
+    bfs(v)
