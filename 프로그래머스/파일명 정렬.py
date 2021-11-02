ddac9b6bf24b570f0d4a2558e995b8bfb619d4a3
RockMiin <52374789+RockMiin@users.noreply.github.com>
2021-11-02 12:33:53 +0900

    Create 파일명 정렬.py

---
+++ 프로그래머스/파일명 정렬.py
@@ -0,0 +1,35 @@
+import re
+
+def solution(files):
+    
+    file_list= []
+    result= []
+    
+    for file in files:
+
+        num= re.findall('[0-9]+', str(file))
+        string= re.findall('[^0-9]+', str(file))
+        
+        file_list.append([string[0], num[0], file[len(string[0])+len(num[0]):]])
+
+        file_list.sort(key = lambda x : (x[0].lower(), int(x[1])))
+        
+    for file in file_list:
+        result.append(''.join(file))
+    
+    return result
+  
+# best
+import re
+
+def solution(files):
+    answer = []
+    files_split = [re.split(r'(\d+)', file) for file in files]
+    files_split.sort(key = lambda x : (x[0].lower(), int(x[1])))
+    
+    for i in files_split:
+        answer.append(''.join(i)) 
+    
+    return answer
+  
+  # REF : https://github.com/naem1023/codingTest/blob/master/sort/pg-30-17686.py
